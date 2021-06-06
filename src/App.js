5f2dfd9a0543e29aecd90981f675b6ae26e6ea45
sachin <sharmascahin066@gmail.com>
2021-06-06 20:49:59 +0530

    App js updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import TableSection from './Comps/Table/TableSection'
+import {BrowserRouter as Router ,Route,Switch} from 'react-router-dom'
+import SingleTableData from './Comps/Table/SingleTableData'
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+<div className="App">
+<Router>
+       <Switch>
+       <Route path="/" exact component={TableSection} />
+         <Route path="/table/:tableId" exact component={SingleTableData} /> 
+        <Route>Error 404 not found</Route>
+       </Switch>
+      </Router>
+</div>
+  )
 }
 
-export default App;
+export default App
+
